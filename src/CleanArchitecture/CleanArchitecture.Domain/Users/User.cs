82bfc6b4bb39c48b69c875edf72974a6266bfede
JoelDiaz93 <cdiaz_jl@hotmail.com.ar>
2024-10-16 16:55:25 -0500

    MediatR

--- src/CleanArchitecture/CleanArchitecture.Domain/Users/User.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Users/User.cs
@@ -1,5 +1,6 @@
 using System.Security.Cryptography.X509Certificates;
 using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Users.Events;
 
 namespace CleanArchitecture.Domain.Users;
 
@@ -7,8 +8,8 @@ public sealed class User : Entity
 {
   private User(
     Guid id,
-    Nombre nombre, 
-    Apellido apellido, 
+    Nombre nombre,
+    Apellido apellido,
     Email email
   ) : base(id)
   {
@@ -22,12 +23,13 @@ public sealed class User : Entity
 
   //Encapsulamos el metodo de creacion de ojetos de mi clase con el constructor como privado y con un metodo estatico publico que se haga cargo en la creacion de los objetos de esta clase
   public static User Create(
-    Nombre nombre, 
-    Apellido apellido, 
+    Nombre nombre,
+    Apellido apellido,
     Email email
   )
   {
-    var user = new User(Guid.NewGuid(), nombre, apellido,email);
+    var user = new User(Guid.NewGuid(), nombre, apellido, email);
+    user.RaiseDomainEvent(new UserCreateDomainEvent(user.Id));
     return user;
   }
 }
